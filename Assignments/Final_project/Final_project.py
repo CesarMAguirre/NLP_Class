9bfa48a9b1ce1f2d7d21bfe27d03b332ad662e13
Cesar Martin Aguirre Aragon <143432293+CesarMAguirre@users.noreply.github.com>
2025-05-10 01:44:44 -0500

    getting the dataset in place

--- Assignments/Final_project/Final_project.py
+++ Assignments/Final_project/Final_project.py
@@ -1,3 +1,8 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import pandas as pd
+
+training_data_path = r"C:\BC\SP_25\NLP\NLP_Class\Assignments\Final_project\pii-detection-removal-from-educational-data\train.json"
+
+training_data = pd.read_json(training_data_path, lines=True)
+print(f'Shape of the training data: {training_data.shape}')
+training_data.head(10)
+
