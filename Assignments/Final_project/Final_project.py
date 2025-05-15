2b8c83438896784cc8ebf774c9a2cc92a2221603
Cesar Martin Aguirre Aragon <143432293+CesarMAguirre@users.noreply.github.com>
2025-05-14 21:35:34 -0500

    Deleted the old jupyter file

--- Assignments/Final_project/Final_project.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-training_data_path = r"C:\BC\SP_25\NLP\NLP_Class\Assignments\Final_project\pii-detection-removal-from-educational-data\train.json"
-
-training_data = pd.read_json(training_data_path, lines=True)
-print(f'Shape of the training data: {training_data.shape}')
-training_data.head(10)
-
